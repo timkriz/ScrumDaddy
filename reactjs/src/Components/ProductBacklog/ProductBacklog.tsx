93ba8198058aebd1b138132fd33a45c4851aaa8b
timkriz <tim.kriznik@gmail.com>
2021-03-14 20:16:47 +0100

    Razvrscanje uporab. zgodb v sprinte...

--- reactjs/src/Components/ProductBacklog/ProductBacklog.tsx
+++ reactjs/src/Components/ProductBacklog/ProductBacklog.tsx
@@ -3,8 +3,10 @@ import "./productbacklog.css";
 import {Button} from "@material-ui/core";
 //import {ISprint, IProject} from "../ProjectList/IProjectList";
 //import {IStory} from "./IStory"
-import {getProjects} from "../../api/ProjectService";
+import {IProjectUser} from "../ProjectList/IProjectList";
+import {getProjects, getProjectUser} from "../../api/ProjectService";
 import {getSprints, getStories} from "../../api/UserStoriesService";
+import {getUserId, getUserRole} from "../../api/TokenService";
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
@@ -15,6 +17,7 @@ import {TabPanel} from "./TabPanel"
 import { useHistory } from "react-router-dom";
 import IconButton from "@material-ui/core/IconButton";
 import {ArrowForwardRounded, DeleteRounded, EditRounded} from "@material-ui/icons";
+import {ProjectRoles, SystemRoles} from "../../data/Roles";
 
 interface IProject {
   _id: string;
@@ -42,21 +45,30 @@ interface IStory {
   projectId: string;
   sprintId: string;
 }
+interface ISprintCollection{ 
+  _id: string; 
+  name: string;
+  stories: IStory[];
+}
 
-const allProjects: IProject[] = [
+const project1: IProject[] = [
     {
-      _id: "6046abf2543fd334f05175e9",
+      _id: "",
       name: "",
       description: ""
     }
   ];
+
 export default () => {
   const [ projects, setProjects ] = useState<IProject[]>([]);
   const [ sprints, setSprints ] = useState<ISprint[]>([]);
   const [ stories, setStories ] = useState<IStory[]>([]);
-  let [selectedProject, setSelectedProject] = React.useState(allProjects[0]);
+  const [ ISprintCollection, setSprintCollection ] = useState<ISprintCollection[]>([]);
+  let [selectedProject, setSelectedProject] = useState<IProject>(project1[0]);
   const [valueTab, setValue] = React.useState(0);
+  const [selectedBtn, setSelectedBtn] = React.useState(-1);
   const history = useHistory();
+  const userRole = getUserRole();
 
   const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
     setValue(newValue);
@@ -66,35 +78,66 @@ export default () => {
     fetchProjects();
   }, []);
 
-  /* Fetch all projects*/
+  /* Fetch all projects */
   const fetchProjects = async () => {
-    const allFetchedProjects = (await getProjects()).data.data as IProject[];
-    setProjects(allFetchedProjects);
-  }
-  /* Fetch all sprints*/
-  const fetchSprints = async (project_id: string) => {
-    const allSprints = (await getSprints(project_id)).data.data as ISprint[];
-    setSprints(allSprints);
-    allSprints.forEach( (sprint) => {
-      fetchStories(sprint._id)
-     });
-  }
-  /* Fetch stories for sprints*/
-  const fetchStories = async (sprint_id: string) => {
-    const allStories = (await getStories(selectedProject._id, sprint_id)).data.data as IStory[];
-    setStories(allStories);
-    console.log(allStories)
+    const userId = getUserId();
+    if(userId !== null) {
+      const allFetchedProjects = (await getProjects()).data.data as IProject[];
+
+      /* Show projects that belong to the user */
+      let shownProjects: IProject[] = [];
+      let shownProjectRoles: (ProjectRoles | null)[] = [];
+
+      for(let i = 0; i < allFetchedProjects.length; i++) {
+        const curProject = allFetchedProjects[i];
+        const gottenProjectUser = (await getProjectUser(curProject._id, userId)).data.data as IProjectUser;
+
+        if(userRole === SystemRoles.ADMIN || gottenProjectUser !== null) {
+          shownProjects.push(curProject);
+          shownProjectRoles.push(gottenProjectUser ? gottenProjectUser.userRole : null);
+        }
+      }
+      setProjects(shownProjects);
+      /* Set initial project to first one*/
+      if(shownProjects[0] != undefined) {
+        setSelectedProject(shownProjects[0])
+        const ISprintCollection = await fetchSprints(shownProjects[0]._id);  
+        setSprintCollection(ISprintCollection);
+      }
+      //setProjectRoles(shownProjectRoles);
+    }
   }
 
-  const projectSelectionClick= (project: IProject) => {
-    setSelectedProject(project);
-    fetchSprints(project._id)
+  /* Fetch all sprints */
+  const fetchSprints = async (project_id: string) => {
+    const allSprints = (await getSprints(project_id)).data.data as ISprint[];
+    const ISprintCollection:ISprintCollection[] = [];
+
+    setSprints(allSprints);
+    allSprints.forEach( async (sprint) => {
+      const found1 = ISprintCollection.some((el:ISprintCollection) => el._id === sprint._id);
+      if (!found1) ISprintCollection.push({ _id: sprint._id, name: sprint.name,  stories: []});
+
+      /* Get stories of a sprint */
+      const allStories = (await getStories(project_id, sprint._id)).data.data as IStory[];
+      const found2 = ISprintCollection.find((el:ISprintCollection) => el._id === sprint._id);
+      if(found2){
+        found2.stories = allStories;
+      }
+      setStories(allStories)
+     });
+     return ISprintCollection
+  }
+ 
+  const projectSelectionClick = async (project: IProject, buttonIn:number) => {
+    setSelectedProject(project); // Update clicked project
+    const ISprintCollection = await fetchSprints(project._id);  
+    setSprintCollection(ISprintCollection); // Update sprint and its stories
+    setSelectedBtn(buttonIn) // Change button color
   }
 
   return (
     <div className="product_backlog_container">
-        <div className="page_title">Product backlog: {selectedProject.name}</div>
-        <hr style={{ margin: "30px 0" }}/>
 
         <Grid container direction="row" justify="center" alignItems="flex-start">
 
@@ -117,7 +160,7 @@ export default () => {
                 >
                     {
                     projects.map((project, i) => (
-                        <Button key={i} onClick={() => projectSelectionClick(project)} style={{justifyContent: "flex-start"}}>{project.name}</Button>
+                        <Button key={i} color={selectedBtn === i ? "secondary" : "primary"} onClick={() => projectSelectionClick(project, i)} style={{justifyContent: "flex-start"}}>{project.name}</Button>
                     ))
                     }
                 </ButtonGroup>
@@ -138,40 +181,61 @@ export default () => {
                         </Tabs>
                     
                     <TabPanel value={valueTab} index={0}>
+                        <div>
+                        <div className="page_subtitle" style={{ marginBottom: 20 }}>Sprint backlog</div>
+                        {/* DIFFERENT SPRINTS */}
                         {
-                        sprints.map((sprint, i) => (
-                          <Typography key={i} component={'span'} variant="h5" style={{marginBottom: "10px"}}>{sprint.name}</Typography>
-                        ))
-                        }
-                        {/*  CARDS OF STORIES */}
-                        {
-                        stories.map((story, i) => (
-                          <div key={i} className="story_row">
-                            <div className="story_row_title">{story.name}</div>
-                            <div className="story_row_title">
-                              <Typography key={i} component={'span'} display = "block" variant="subtitle2" className="story_row_status">Status: {story.status}</Typography>
-                              <Typography key={i} component={'span'} display = "block" variant="subtitle2" className="story_row_status">Priority: {story.priority}</Typography>
-                              <Typography key={i} component={'span'} display = "block" variant="subtitle2" className="story_row_status">Business Value: {story.businessValue}</Typography>
+                          ISprintCollection.map((sprint, i) => (
+                            <div key={i}>
+                              <Typography key={i} variant="h6">{sprint.name}</Typography>
+
+                              <div>
+                                <Typography key={i} component={'span'} display = "block" variant="subtitle2" className="story_row_status">
+                                  {sprint.stories === undefined || sprint.stories.length == 0 ? 'No stories in sprint' : ''}
+                                </Typography>
+                                </div>
+                              <div>
+                              {/* DIFFERENT STORY CARDS */}
+                              {
+                                sprint.stories.map((story, j) =>(
+                                  <div key={j} className="story_row">
+                                    <div className="story_row_title">{story.name}</div>
+                                    <div className="story_row_title">
+                                      <Typography key={j} component={'span'} display = "block" variant="subtitle2" className="story_row_status">Status: {story.status}</Typography>
+                                      <Typography key={j} component={'span'} display = "block" variant="subtitle2" className="story_row_status">Priority: {story.priority}</Typography>
+                                      <Typography key={j} component={'span'} display = "block" variant="subtitle2" className="story_row_status">Business Value: {story.businessValue}</Typography>
+                                    </div>
+                                    <div className="story_row_icons">
+                                      <IconButton color="primary">
+                                        <DeleteRounded />
+                                      </IconButton>
+                                      <IconButton color="primary">
+                                        <EditRounded />
+                                      </IconButton>
+                                      <IconButton color="primary">
+                                        <ArrowForwardRounded />
+                                      </IconButton>
+                                    </div>
+                                  </div>
+                                  ))
+                              }
+                              </div>
                             </div>
-                            <div className="story_row_icons">
-                              <IconButton color="primary">
-                                <DeleteRounded />
-                              </IconButton>
-                              <IconButton color="primary">
-                                <EditRounded />
-                              </IconButton>
-                              <IconButton color="primary">
-                                <ArrowForwardRounded />
-                              </IconButton>
-                            </div>
-                          </div>
-                        ))
+                          ))
                         }
-                        <p>Not assigned to sprints</p>
+                        </div>
+                        <hr style={{ margin: "30px 0" }}/>
+                        <div className="page_subtitle" style={{ marginBottom: 20 }}>Product backlog</div>
+
+                        <Typography component={'span'} display = "block" variant="subtitle2" className="story_row_status">
+                                  Not assigned to sprints yet
+                        </Typography>
                         <Button variant="contained" color="primary" style={{ alignSelf: "flex-start" }}>ADD USER STORY</Button>
                     </TabPanel>
                     <TabPanel value={valueTab} index={1}>
-                        <p>Realized stories</p>
+                        <Typography component={'span'} display = "block" variant="subtitle2" className="story_row_status">
+                                  Needs an new endpoint i think
+                        </Typography>
                     </TabPanel>
                 </div>
                 </Paper>
