65fa218cd9fc0489e849005ae3c4fee48441c1c8
Luka Železnik <luka.zeleznik10@gmail.com>
2021-03-18 17:35:06 +0100

    oops

--- nodejs/db.js
+++ nodejs/db.js
@@ -17,8 +17,8 @@ const options = {
 };
 
 //const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
-//const url = process.env.MONGODB_CLOUD_URI;
-const url = 'mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb'
+const url = process.env.MONGODB_CLOUD_URI;
+//const url = 'mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb'
 
 mongoose.connect(url, options).then( function() {
   console.log('MongoDB is connected');
